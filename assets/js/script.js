453801f0b24b5872d374cb9b8dd7488e23e17a02
Fabulosa <fabiscg79@gmail.com>
2021-11-28 23:48:14 -0500

    Trying to make the Icon fit in the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var searchBtn = document.querySelector('#search-button')
 
 
 
+
+
 searchBtn.addEventListener('click', getCity)
 
 function getCity(e) {
@@ -23,15 +25,19 @@ function getCurrent(cityName) {
             var lon = data.coord.lon
             fiveDayForecast(lat, lon)
 
-            
+           
             
 
+
             console.log('current', data);
+
+            var iconImage = document.querySelector('#img')
             document.querySelector('#city-name').textContent = data.name
             document.querySelector('#temp').textContent = 'Temp: ' + data.main.temp + ' °C'
-            document.querySelector('#icon').textContent = data.weather[0].icon
             document.querySelector('#wind').textContent = 'Wind Speed: ' + data.wind.speed + ' MPH'
             document.querySelector('#humidity').textContent = 'Humidity: ' + data.main.humidity
+
+
         })
 }
 
@@ -46,7 +52,8 @@ function fiveDayForecast(lat, lon) {
             console.log('5 day', data)
 
             document.querySelector('#uv').textContent = 'UV: ' + data.current.uvi
-
+            
+          
             for (var i = 0; i < 5; i++) {
 
                 var card = document.createElement('div')
@@ -57,25 +64,37 @@ function fiveDayForecast(lat, lon) {
                 date.textContent = moment().add(i + 1, 'days').format('MMM Do YY')
                 card.prepend(date)
 
-                card.setAttribute('id','img')
-                var iconUrl = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
-                iconUrl.textContent = data.daily[i].weather.icon
-                card.append(iconUrl)
 
                 var fiveDayTemp = document.createElement('p')
                 fiveDayTemp.textContent = 'Temp: ' + data.daily[i].temp.day
                 card.append(fiveDayTemp)
 
+            
+
                 var fiveDayWind = document.createElement('p')
-                fiveDayWind.textContent =  'Wind: ' + data.daily[i].wind_speed + 'MPH'
+                fiveDayWind.textContent = 'Wind: ' + data.daily[i].wind_speed + 'MPH'
                 card.append(fiveDayWind)
 
                 var fiveDayHumidity = document.createElement('p')
                 fiveDayHumidity.textContent = 'Humidity:' + data.daily[i].humidity + '%'
                 card.append(fiveDayHumidity)
 
-                
+
 
             }
         })
+
+        function getIcon(){
+            fetch( "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + "@2x.png")
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function(data){
+                console.log(getIcon);
+            })
+            
+            // var iconImage =
+            //     var icon = document.querySelector('img').setAttribute("src", iconImage)
+            //     card.append(icon)
+        }
 }
